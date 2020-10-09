ae09ea2f9923fc37e0dcaba19ab32467a92a86ea
Christian Gonzalez <cgonzalez233@yahoo.com>
2020-10-09 12:34:41 -0400

    Script runs through questions

--- script.js
+++ script.js
@@ -1,15 +1,94 @@
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-var timerStart = 6
+var timerDisplay = document.querySelector("#timer")
+var timerStart = 60
 
-function timerFunction(){
-    var timer = setInterval(function(timerStart){
-        timerStart--
-        console.log("Something")
-    }, 1000);
-}
 
-timerFunction();
+var quesDisplay = document.querySelector("#quesDisplay")
+var ansDisplay1 = document.querySelector("#ansDisplay1")
+var ansDisplay2 = document.querySelector("#ansDisplay2")
+var ansDisplay3 = document.querySelector("#ansDisplay3")
+var ansDisplay4 = document.querySelector("#ansDisplay4")
+
+
+// function timerFunction(){
+//     var timer = setInterval(function(){
+//         if (timerStart > 0){
+//             timerDisplay.textContent = timerStart;
+//             timerStart--;
+//             timerDisplay.textContent = timerStart;
+//             if (timerStart === 0){
+//                 alert("Yay");
+//             }
+//         }
+//     }, 1000);
+// }
+
+
+var btn = document.querySelector("#submit")
+
+var quizData = [
+    {
+        question: "Question 1",
+        answer1: "Answer 1",
+        answer2: "Answer 2",
+        answer3: "Answer 3",
+        answer4: "Answer 4",
+        correct: "Answer 1"
+    }, {
+        question: "Question 2",
+        answer1: "Answer A",
+        answer2: "Answer B",
+        answer3: "Answer C",
+        answer4: "Answer D",
+        correct: "Answer B"
+    },{
+        question: "Question 3",
+        answer1: "Answer One",
+        answer2: "Answer Two",
+        answer3: "Answer Three",
+        answer4: "Answer Four",
+        correct: "Answer Four"
+    },{
+        question: "Question 4",
+        answer1: "Answer 1",
+        answer2: "Answer 2",
+        answer3: "Answer 3",
+        answer4: "Answer 4",
+        correct: "Answer 3"
+    }
+];
+
+quizNum = 0
+
+function quesFunction (){
+    var currentQuiz = quizData[quizNum]
+    quesDisplay.textContent = currentQuiz.question
+    ansDisplay1.textContent = currentQuiz.answer1
+    ansDisplay2.textContent = currentQuiz.answer2
+    ansDisplay3.textContent = currentQuiz.answer3
+    ansDisplay4.textContent = currentQuiz.answer4
+    quizNum++
+
+};
+
+btn.addEventListener("click", function(){
+    if (quizNum < quizData.length){
+        btn.textContent = "Submit"
+        quesFunction()
+    }else {
+        var userResponse = prompt("You finished! Please enter your name below!")
+        btn.textContent = "Restart"
+        quizNum = 0
+
+
+
+    }
+});
+
+
+
+
 
 // WHEN I answer a question
 // THEN I am presented with another question
